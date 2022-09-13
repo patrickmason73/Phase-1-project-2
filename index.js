20509cda1a20db3145d875fc97b08af94abfb728
Patrick Mason <patrickmason73@gmail.com>
2022-09-12 18:08:50 -0600

    trial and error but changed display function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const init = () => {
-
+    document.addEventListener('DOMContentLoaded', init);
 }
 
-document.addEventListener('DOMContentLoaded', init);
 
-fetch("https://jikan.moe")
+fetch("https://api.jikan.moe/v4/anime")
     .then((response) => {
         if (response.ok) {
             return response.json();
@@ -13,14 +12,24 @@ fetch("https://jikan.moe")
           }
         })
     .then(data => {
-          document.getElementById('searchResults').style.display = 'Block'
-          = data
+        console.log(data); 
+        displaySearch(data);
         })
     .catch((error) => console.error("FETCH ERROR:", error));   
+ 
+    function displaySearch(data) {
+        const animeDiv = document.getElementById("anime")
+        data.array.forEach(anime => {
+            const animeElement = document.createElement('p')
+            animeElement.innerText = `Anime Name: ${anime.name}`
+            animeDiv.append(animeElement)
+        });
+        heading.innerHTML = anime;
+        animeDiv.appendChild(heading);
+    }
 
     const clearButton = document.getElementById('clear')
     function clearList(){
-
         while (list.firstChild){
             list.removeChild(list.firstChild)
         }
