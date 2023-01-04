d052f2d60f4efba145883c483e6eeb551f3fbaa7
Patrick Mason <patrickmason73@gmail.com>
2023-01-03 21:44:44 -0700

    final touches, added a seach box if pokemon is not in dropdown

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
+//DOMContentLoaded callback function
+
 const init = () => {
     const inputForm = document.querySelector('form');
   
     inputForm.addEventListener('submit', (event) => {
       event.preventDefault();
-        console.log(event);
+        
         
         
     });
 const myDropdown = document.getElementById("myDropdown")
-console.log(myDropdown)
+
+
+
+
+
 
 myDropdown.addEventListener("change", (event) => 
 {console.log(event.target.value)
   fetchPokemon(event);
+  
 }
+
 )
+
   }
   
   document.addEventListener('DOMContentLoaded', init)
-  function dropdownFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+  // document.addEventListener('submit', () => {
+  
+  // return document.getElementById("myDropdown").classList.toggle("show")
+  // })
   
 
 
@@ -39,6 +49,65 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${event.target.value}`)
         })
     .catch((error) => console.error("FETCH ERROR:", error));   
   }
+
+
+
+function postPokemon(event) {
+  event.preventDefault()
+let newPokemonName = document.getElementById('newPokemonName')
+let newData = newPokemonName.value
+  fetch(`https://pokeapi.co/api/v2/pokemon/${newData}`)
+  .then(res => res.json())
+  .then(data => {
+    let stat = document.getElementById("stat")
+    let movesNameArr = []
+    let toBeDeleted = document.getElementsByTagName("ul")
+    let ul = document.createElement("ul")
+    let toBeDeletedImg = document.getElementById("newImg")
+    let toBeDeletedImg2 = document.getElementById("shinyImg")
+
+    if (toBeDeletedImg2 != null) {
+      toBeDeletedImg2.remove()
+    }
+
+    if (toBeDeletedImg != null ) {
+      toBeDeletedImg.remove();
+    }
+    
+    while (toBeDeleted.length > 0) {
+      toBeDeleted[0].remove()
+    }
+    data.moves.forEach((item) => {
+      movesNameArr.push(item.move.name)
+    })
+    movesNameArr.forEach((item) => {
+      let listElement = document.createElement('li')
+      listElement.textContent = item
+      stat.appendChild(ul)
+      ul.appendChild(listElement)
+    })
+    
+    let img = document.createElement('img')
+    img.id = "newImg"
+    img.src = data.sprites.front_default
+    document.getElementById('body').appendChild(img) 
+
+    let img2 = document.createElement('img')
+      img2.id = "shinyImg"
+      img2.src = data.sprites.front_shiny
+    let shinyPic = document.getElementById("shinyPic")
+    let shinyFunction = document.getElementById("shinyFunction")
+    shinyPic.appendChild(img2)
+    shinyFunction.addEventListener("click", () => {
+      img2.style.display = "block"
+      img2.style.height = 300
+      img2.style.width = 300
+      })
+    
+  })}
+
+const insertPokemon = document.getElementById('insertPokemon')
+insertPokemon.addEventListener('submit', postPokemon)
  
 function displayPokemon(data) {
     
@@ -81,28 +150,12 @@ function displayPokemon(data) {
     let shinyPic = document.getElementById("shinyPic")
     let shinyFunction = document.getElementById("shinyFunction")
     shinyPic.appendChild(img2)
-    shinyFunction.addEventListener("click", (data) => {
+    shinyFunction.addEventListener("click", () => {
       img2.style.display = "block"
       img2.style.height = 300
       img2.style.width = 300
       })
     
   }
-  
-  
-  
-    
-
-
-
-
-
-
-
-
-
-
-
-
 
 
